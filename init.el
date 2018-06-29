d2173205cf5dd490664173b91b39bd994eb88fa0
Leo Vivier <leo.vivier@gmail.com>
2018-06-29 11:32:19 +0200

    Add function to save window configuration before opening mu4e links

--- init.el
+++ init.el
@@ -2938,6 +2938,13 @@ i.e. change right window to bottom, or change bottom window to right."
 	  (setq zp/mu4e-before-config (current-window-configuration))
 	  (mu4e)))))
 
+(defun zp/mu4e-view-message-with-message-id-save-window-config-before (old-function &rest arguments)
+  (interactive)
+  (setq zp/mu4e-before-config (current-window-configuration))
+  (apply old-function arguments))
+
+(advice-add #'mu4e-view-message-with-message-id :around #'zp/mu4e-view-message-with-message-id-save-window-config-before)
+
 (defun zp/ispell-switch-dictionary ()
   (interactive)
   (if (eq ispell-local-dictionary nil)
