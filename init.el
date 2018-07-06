ecf3e7c1b8e260ba285ad6d3e4b41716266b52af
Leo Vivier <leo.vivier@gmail.com>
2018-07-06 14:01:48 +0200

    Advice mu4e-quit to check-mail

--- init.el
+++ init.el
@@ -3048,13 +3048,14 @@ i.e. change right window to bottom, or change bottom window to right."
   (if (string-match "*mu4e-.*" (buffer-name))
       (progn
 	(mu4e-quit)
-	(mu4e-update-mail-and-index t)
-	;; (delete-other-windows)
 	(set-window-configuration zp/mu4e-before-config))
     (if (eq (get-buffer "*mu4e-main*") nil)
 	(progn
 	  (setq zp/mu4e-before-config (current-window-configuration))
 	  (mu4e)))))
+
+(advice-add #'mu4e-quit :after (lambda ()
+				 (mu4e-update-mail-and-index t)))
 
 (defun zp/mu4e-view-message-with-message-id-save-window-config-before (old-function &rest arguments)
   (interactive)
