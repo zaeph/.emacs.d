cbb0f59f4ebf78de269cb199c3ed674649a70b8d
Leo Vivier <leo.vivier@gmail.com>
2018-07-24 09:40:09 +0200

    Update MELPA implementation to package-archives
    Based on wiki.

--- init.el
+++ init.el
@@ -58,15 +58,17 @@
 ;; ========================================
 
 ;; MELPA
-(require 'package) ;; You might already have this line
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") 1)
-(add-to-list 'package-archives
-             '("org-elpa" . "http://orgmode.org/elpa/") 1)
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
 
 ;; Load extra files, and search subdirs
 (let ((default-directory  "/home/zaeph/.emacs.d/lisp/"))
