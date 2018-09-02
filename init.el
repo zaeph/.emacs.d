81ae93d38ba032718965fa8402148206bf0e1a4d
Leo Vivier <leo.vivier@gmail.com>
2018-09-02 17:33:06 +0200

    Tweak ERC variables

--- init.el
+++ init.el
@@ -351,11 +351,21 @@
 ;; ========================================
 
 (require 'erc)
-(setq erc-autojoin-channels-alist
+(setq erc-autojoin-timing 'ident
+      erc-autojoin-channels-alist
       '(("freenode.net" "#emacs" "#ranger")
 	("myanonamouse.net" "#anonamouse.net" "#am-members"))
       erc-join-buffer 'bury
-      erc-hide-list '("JOIN" "PART" "QUIT"))
+      erc-fill-function 'erc-fill-static
+      erc-fill-static-center 22
+      erc-hide-list '("JOIN" "PART" "QUIT")
+      erc-lurker-hide-list '("JOIN" "PART" "QUIT")
+      erc-lurker-threshold-time 43200
+      erc-prompt-for-nickserv-password nil
+      erc-server-reconnect-attempts 5
+      erc-server-reconnect-timeout 3
+      erc-track-exclude-types '("JOIN" "MODE" "NICK" "PART" "QUIT"
+				 "324" "329" "332" "333" "353" "477"))
 
 (add-to-list 'erc-modules 'notify)
 (add-to-list 'erc-modules 'notifications)
