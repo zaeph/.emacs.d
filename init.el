88f9a17f1ee67eaea438010654ff56df724ed8e4
Leo Vivier <leo.vivier@gmail.com>
2018-09-20 10:05:57 +0200

    Fix indent

--- init.el
+++ init.el
@@ -1156,9 +1156,9 @@ If text is selected, adds furigana to the selected kanji instead."
 (setq backup-directory-alist `(("." . "/home/zaeph/.saves")))
 (setq backup-by-copying t)
 (setq delete-old-versions t
-  kept-new-versions 6
-  kept-old-versions 2
-  version-control t)
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
 
 
 ;; ========================================
