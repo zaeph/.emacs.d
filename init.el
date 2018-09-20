77d2e44bb10a8db7cf4d6946922607dab8249f18
Leo Vivier <leo.vivier@gmail.com>
2018-09-20 10:35:49 +0200

    Fix regex match for ‘Stuck projects’ header

--- init.el
+++ init.el
@@ -2190,7 +2190,7 @@ agenda settings after them."
 
 (defun zp/org-agenda-hi-lock ()
   (highlight-regexp "([-+].*?)" 'zp/org-agenda-block-info-face)
-  (highlight-regexp "^[[:space:]]*? \\[ Stuck Projects \\]$" 'zp/org-agenda-block-warning-face))
+  (highlight-regexp "^[[:space:]]*? \\[ Stuck Projects \\]" 'zp/org-agenda-block-warning-face))
 
 (defun zp/org-agenda-remove-mouse-face ()
   "Remove mouse-face from org-agenda."
