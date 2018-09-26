debd620b006ca7792f25664f4e8bf0ea97889eac
Leo Vivier <leo.vivier@gmail.com>
2018-09-26 23:07:15 +0200

    Replace dired+ with dired-x

--- init.el
+++ init.el
@@ -217,9 +217,23 @@
 ;; (define-key Info-mode-map (kbd "h") 'Info-history-back)
 ;; (define-key Info-mode-map (kbd "l") 'Info-history-forward)
 
-(setq diredp-hide-details-initially-flag nil)
-(require 'dired+)
+;; dired+
+;; (setq diredp-hide-details-initially-flag nil)
+;; (require 'dired+)
 
+;; dired-x
+(add-hook 'dired-load-hook
+          (lambda ()
+            (load "dired-x")
+            ;; Set dired-x global variables here.  For example:
+            ;; (setq dired-guess-shell-gnutar "gtar")
+            ;; (setq dired-x-hands-off-my-keys nil)
+            ))
+(add-hook 'dired-mode-hook
+          (lambda ()
+            ;; Set dired-x buffer-local variables here.  For example:
+            ;; (dired-omit-mode 1)
+            ))
 
 (require 'recentf-ext)
 
