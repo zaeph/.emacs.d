d7f22024146852c1b49164eeced4e1ed4921ad5d
Leo Vivier <leo.vivier@gmail.com>
2018-12-07 15:48:18 +0100

    Implement ivy

--- init.el
+++ init.el
@@ -1804,8 +1804,17 @@ return `nil'."
 
 ;; (zp/set-shortcuts-all)
 
-;; Helm
+;; ========================================
+;; ================= HELM =================
+;; ========================================
+
 (require 'helm)
+
+;; (helm-mode 1)
+
+;; Disable helm-mode for given functions
+;; Used to be necessary, but now it works just fine
+;; (add-to-list 'helm-completing-read-handlers-alist '(org-set-property))
 
 (define-prefix-command 'zp/helm-map)
 (global-set-key (kbd "C-c h") 'zp/helm-map)
@@ -1848,6 +1857,38 @@ return `nil'."
 ;; Helm Projectile
 
 (global-set-key (kbd "C-c h p") 'helm-projectile)
+
+
+
+;; ========================================
+;; ================= IVY ==================
+;; ========================================
+
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(setq ivy-count-format "(%d/%d) "
+      ivy-height 10			;Default
+      counsel-find-file-at-point t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "C-x C-b") 'ivy-switch-buffer)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "<menu>") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
 
 
 
@@ -3561,19 +3602,6 @@ on init and them removes itself."
 
 (defun zp/play-sound-turn-page ()
   (start-process-shell-command "play-sound" nil "notification-sound-org page"))
-
-
-
-;; ========================================
-;; ================ HELM ==================
-;; ========================================
-
-
-(helm-mode 1)
-
-;; Disable helm-mode for given functions
-;; Used to be necessary, but now it works just fine
-;; (add-to-list 'helm-completing-read-handlers-alist '(org-set-property))
 
 
 
