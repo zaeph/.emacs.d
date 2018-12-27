ca2717f96516f1e8553705b9684ebce5167bc2f8
Leo Vivier <zaephon@gmail.com>
2018-12-27 19:42:54 +0100

    Revert to helm-M-x from counsel-M-x

--- init.el
+++ init.el
@@ -2041,7 +2041,7 @@ return `nil'."
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "<f6>") 'ivy-resume)
 ;; (global-set-key (kbd "C-x C-b") 'ivy-switch-buffer)
-(global-set-key (kbd "M-x") 'counsel-M-x)
+;; (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "<menu>") 'counsel-M-x)
 ;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "<f1> f") 'counsel-describe-function)
