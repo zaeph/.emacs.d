d41cc8ae2dd91e303b98ff1476c8b92d9722f74c
Leo Vivier <zaephon@gmail.com>
2019-01-22 12:01:56 +0100

    Add icon for inbox

--- init.el
+++ init.el
@@ -1726,6 +1726,7 @@ return `nil'."
         ("^elisp$" "~/org/svg/icons/spacemacs.svg" nil nil :ascent center)
         ("^linux$" "~/org/svg/icons/nixos.svg" nil nil :ascent center)
         ("^opsec$" "~/org/svg/icons/cyber-security-b.svg" nil nil :ascent center)
+        ("^inbox$" "~/org/svg/icons/gmail.svg" nil nil :ascent center)
         ("^awakening$" "~/org/svg/icons/aperture-green.svg" nil nil :ascent center)
         ("^hack$" "~/org/svg/icons/engineering-2.svg" nil nil :ascent center)
         ("^python$" "~/org/svg/icons/python.svg" nil nil :ascent center)
