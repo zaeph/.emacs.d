516c8bc33b567060c800456405c06a412149caf1
Leo Vivier <leo.vivier+dev@gmail.com>
2019-07-17 10:29:51 +0200

    Initialise use-package after MELPA

--- init.el
+++ init.el
@@ -138,6 +138,12 @@
 ;; Initialise packages
 (package-initialize)
 
+;; ‘use-package’ initialisation
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  ;; (add-to-list 'load-path "<path where use-package is installed>")
+  (require 'use-package))
+
 ;; org-elpa
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 
