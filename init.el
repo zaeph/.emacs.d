fb91eeeffaf7a87cc1fac21f0afb3daa1b592427
Leo Vivier <leo.vivier+dev@gmail.com>
2019-07-18 08:45:12 +0200

    Remove old ‘Ediff in dired’ block

--- init.el
+++ init.el
@@ -6394,40 +6394,6 @@ Version 2017-08-25"
 
 
 
-
-
-
-
-
-
-;; Ediff in dired
-;; https://oremacs.com/2017/03/18/dired-ediff/
-;; (require 'dired)
-;; (setq dired-dwim-target t)
-;; (defun ora-ediff-files ()
-;;   (interactive)
-;;   (let ((files (dired-get-marked-files))
-;;         (wnd (current-window-configuration)))
-;;     (if (<= (length files) 2)
-;;         (let ((file1 (car files))
-;;               (file2 (if (cdr files)
-;;                          (cadr files)
-;;                        (read-file-name
-;;                         "file: "
-;;                         (dired-dwim-target-directory)))))
-;;           (if (file-newer-than-file-p file1 file2)
-;;               (ediff-files file2 file1)
-;;             (ediff-files file1 file2))
-;;           (add-hook 'ediff-after-quit-hook-internal
-;;                     (lambda ()
-;;                       (setq ediff-after-quit-hook-internal nil)
-;;                       (set-window-configuration wnd))))
-;;       (error "no more than 2 files should be marked"))))
-
-;; (define-key dired-mode-map "e" 'ora-ediff-files)
-
-
-
 ;; ========================================
 ;; ============ PSYCHOTHERAPY =============
 ;; ========================================
