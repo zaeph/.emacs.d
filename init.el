1a3f31179a20b0a5c1a7672eaea8fae1c8b02a98
Leo Vivier <leo.vivier+dev@gmail.com>
2019-07-20 20:43:35 +0200

    Add lexical binding in init.el
    Not sure why it was removed.

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; -*- fill-column: 78; comment-column: 50; -*-
+;; -*- fill-column: 78; comment-column: 50; lexical-binding: t; -*-
 ;;; init.el --- Initialization file for Emacs
 ;;; Commentary: Emacs Startup File --- initialization for Emacs
 
