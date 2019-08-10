b46fd9bcfa07911ba8008dff8e3b79e3fa6db02d
Leo Vivier <leo.vivier+dev@gmail.com>
2019-08-10 22:11:55 +0200

    Fix highlight-indent-guides init

--- init.el
+++ init.el
@@ -912,12 +912,12 @@ With numeric prefix arg DEC, decrement the integer by DEC amount."
     (increment-integer-at-point (- (or dec 1)))))
 
 (use-package highlight-indent-guides
-  :bind (:map zp/toggle-map
-              ("c" . highlight-indent-guides-mode))
-  :config
-  (setq highlight-indent-guides-method 'column
-        highlight-indent-guides-auto-character-face-perc 20)
-  (add-hook 'prog-mode-hook #'highlight-indent-guides-mode))
+    :bind (:map zp/toggle-map
+                ("c" . highlight-indent-guides-mode))
+    :hook (prog-mode . highlight-indent-guides-mode)
+    :config
+    (setq highlight-indent-guides-method 'column
+          highlight-indent-guides-auto-character-face-perc 20))
 
 ;;----------------------------------------------------------------------------
 ;; Shortcuts
