95e805fb8631aa2fd200f3ba0cf145ec8963bc99
Leo Vivier <leo.vivier+dev@gmail.com>
2019-08-10 19:19:56 +0200

    Map ivy

--- init.el
+++ init.el
@@ -2884,6 +2884,7 @@ indirect-buffers."
           ("C-c C-x d" . zp/org-agenda-delete-property)
           ("C-c C-x s" . zp/org-agenda-wipe-local-config)
           (">" . zp/org-agenda-date-prompt-and-update-appt)
+          ("<" . zp/ivy-org-agenda-groups-set-extra-filters)
           ("C-c C-s" . zp/org-agenda-schedule-and-update-appt)
           ("C-c C-S-w" . zp/org-agenda-refile-with-paths)
           ("Z" . org-resolve-clocks)
