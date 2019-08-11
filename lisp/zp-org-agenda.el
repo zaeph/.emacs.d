f236845a56e08ea57bb3b3272650f3014af254a8
Leo Vivier <leo.vivier+dev@gmail.com>
2019-08-11 19:28:25 +0200

    Change highlight-regexp

--- lisp/zp-org-agenda.el
+++ lisp/zp-org-agenda.el
@@ -1037,7 +1037,7 @@ agenda settings after them."
 
 (defun zp/org-agenda-hi-lock ()
   (highlight-regexp "([-+].*?)" 'zp/org-agenda-block-info-face)
-  (highlight-regexp "F.:{.*?}" 'zp/org-agenda-block-info-face)
+  (highlight-regexp "{.*?}" 'zp/org-agenda-block-info-face)
   ;; (highlight-regexp "^[[:space:]]*? \\[ Stuck Projects \\]" 'zp/org-agenda-block-warning-face)
   (highlight-regexp "^~~.*~~$" 'font-lock-comment-face))
 
