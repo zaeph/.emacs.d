c4c491d09bd4d120547b5ab4ece063d25b88268e
Leo Vivier <leo.vivier+dev@gmail.com>
2019-11-11 00:42:52 +0100

    pdf: Change default colour for annotation to blue

--- init.el
+++ init.el
@@ -785,6 +785,8 @@ Modifies ‘diff-command’ and ‘diff-switches’ to use ‘git diff’."
   (defvar zp/pdf-annot-default-annotation-color "#389BE6"
     "Default color to use for annotations.")
 
+  (setq zp/pdf-annot-default-annotation-color "#389BE6")
+
   (setq pdf-annot-default-annotation-properties
         `((t (label . ,user-full-name))
           (text (icon . "Note") (color . zp/pdf-annot-default-annotation-color))
