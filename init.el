1b59c3335965b3a82e31728606401355990bf820
Leo Vivier <zaephon@gmail.com>
2020-04-20 10:41:47 +0200

    Fix docstring

--- init.el
+++ init.el
@@ -347,7 +347,7 @@ time is displayed."
 ;; Editing commands
 ;;----------------------------------------------------------------------------
 (defun zp/unfill-document ()
-  "fill individual paragraphs with large fill column"
+  "Fill individual paragraphs with large fill column."
   (interactive)
   (let ((fill-column 100000))
     (fill-individual-paragraphs (point-min) (point-max))))
