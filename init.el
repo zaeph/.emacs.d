56eb3416fd824333c9e2b08c85f50fd2bc0ca8ab
Leo Vivier <zaephon@gmail.com>
2020-04-29 12:53:25 +0200

    projectile: Make default action open root in dired

--- init.el
+++ init.el
@@ -3121,12 +3121,39 @@ along with effort estimates and total time."
                                     "~/.bin/"
                                     "~/.dotfiles/"
                                     "~/projects/"))
+  (projectile-switch-project-action (lambda () (dired ".")))
   :config
   (projectile-mode))
 
 (use-package counsel-projectile
   :ensure t
   :bind (("H-p" . counsel-projectile))
+  :custom
+  (counsel-projectile-switch-project-action
+   '(1
+     ("D" counsel-projectile-switch-project-action-dired "open project in dired")
+     ("o" counsel-projectile-switch-project-action "jump to a project buffer or file")
+     ("f" counsel-projectile-switch-project-action-find-file "jump to a project file")
+     ("d" counsel-projectile-switch-project-action-find-dir "jump to a project directory")
+     ("b" counsel-projectile-switch-project-action-switch-to-buffer "jump to a project buffer")
+     ("m" counsel-projectile-switch-project-action-find-file-manually "find file manually from project root")
+     ("S" counsel-projectile-switch-project-action-save-all-buffers "save all project buffers")
+     ("k" counsel-projectile-switch-project-action-kill-buffers "kill all project buffers")
+     ("K" counsel-projectile-switch-project-action-remove-known-project "remove project from known projects")
+     ("c" counsel-projectile-switch-project-action-compile "run project compilation command")
+     ("C" counsel-projectile-switch-project-action-configure "run project configure command")
+     ("E" counsel-projectile-switch-project-action-edit-dir-locals "edit project dir-locals")
+     ("v" counsel-projectile-switch-project-action-vc "open project in vc-dir / magit / monky")
+     ("sg" counsel-projectile-switch-project-action-grep "search project with grep")
+     ("si" counsel-projectile-switch-project-action-git-grep "search project with git grep")
+     ("ss" counsel-projectile-switch-project-action-ag "search project with ag")
+     ("sr" counsel-projectile-switch-project-action-rg "search project with rg")
+     ("xs" counsel-projectile-switch-project-action-run-shell "invoke shell from project root")
+     ("xe" counsel-projectile-switch-project-action-run-eshell "invoke eshell from project root")
+     ("xt" counsel-projectile-switch-project-action-run-term "invoke term from project root")
+     ("xv" counsel-projectile-switch-project-action-run-vterm "invoke vterm from project root")
+     ("Oc" counsel-projectile-switch-project-action-org-capture "capture into project")
+     ("Oa" counsel-projectile-switch-project-action-org-agenda "open project agenda")))
   :init
   (counsel-projectile-mode))
 
