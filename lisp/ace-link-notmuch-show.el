8f6298901c86559fed2f2dc417cf295c97dedf1b
Leo Vivier <leo.vivier+dev@gmail.com>
2020-05-09 12:59:42 +0200

    Improve header

--- lisp/ace-link-notmuch-show.el
+++ lisp/ace-link-notmuch-show.el
@@ -1,8 +1,34 @@
 ;;; ace-link-notmuch-show.el --- Library for opening links in notmuch-show  -*- fill-column: 78; lexical-binding: t; -*-
+
+;; Copyright © 2020 Leo Vivier <leo.vivier+dev@gmail.com>
+
+;; Author: Leo Vivier <leo.vivier+dev@gmail.com>
+;; Version: 0.1.0
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation; either version 3, or (at your option) any later
+;; version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+;; more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with GNU Emacs; see the file COPYING.  If not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+;; USA.
+
 ;;; Commentary:
+;;
+;; This library provides a ace-link handler for links displayed in
+;; a notmuch-show buffer.
 
 ;;; Code:
-
+;;;; Library Requires
 (require 'ace-link)
 (require 'subr-x)
 (require 'shr)
