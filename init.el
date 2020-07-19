131a23be8dad2dba3c5dd5a58655335c4c80b67b
Leo Vivier <leo.vivier+dev@gmail.com>
2020-07-19 11:59:24 +0200

    Add section for Java

--- init.el
+++ init.el
@@ -2185,6 +2185,20 @@ SEARCH is a string to be interpreted by notmuch-search."
       (shell-command (concat "racket " (buffer-file-name))))))
 
 ;;----------------------------------------------------------------------------
+;; Java
+;;----------------------------------------------------------------------------
+
+(use-package lsp-mode :hook ((lsp-mode . lsp-enable-which-key-integration))
+  :config (setq lsp-completion-enable-additional-text-edit nil))
+(use-package company)
+(use-package lsp-ui)
+(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
+(use-package dap-mode :after lsp-mode :config (dap-auto-configure-mode))
+(use-package dap-java :ensure nil)
+(use-package helm-lsp)
+(use-package lsp-treemacs)
+
+;;----------------------------------------------------------------------------
 ;; AUCTeX
 ;;----------------------------------------------------------------------------
 (use-package latex
