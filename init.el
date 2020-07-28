6783559367440986ba2e32cd43e51c52dfb1f27d
Leo Vivier <leo.vivier+dev@gmail.com>
2020-07-28 20:56:26 +0200

    org-journal: Fix disabling

--- init.el
+++ init.el
@@ -4193,7 +4193,7 @@ commas and space."
         org-roam-server-network-label-wrap-length 20))
 
 (use-package org-journal
-  :disabled t
+  :disabled
   :load-path "~/projects/org-journal/"
   :bind (:map org-journal-mode-map
          ("C-c C-f" . org-journal-next-entry)
