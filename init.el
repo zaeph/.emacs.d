ee4c2f51be655d60c4cba30039d3aa8e3f88a2d9
Leo Vivier <leo.vivier+dev@gmail.com>
2020-07-31 23:49:35 +0200

    op: Update templates

--- init.el
+++ init.el
@@ -3646,10 +3646,10 @@ KEY is the key to use to access the template"
 
           ;; Org-protocol templates
           ("OPg" "Capture with guessed action" entry (file+headline "~/org/life.org" "Inbox")
-           "* TODO %(zp/org-protocol-get-verb \"%:link\") %a%? :online:\n%(zp/org-protocol-insert-selection-dwim \"%i\")" :add-created t)
+           "* TODO %(zp/org-protocol-process \"%:link\" \"%:description\")%? :online:\n%(zp/org-protocol-insert-selection-dwim \"%i\")" :add-created t)
 
           ("OPc" "Capture with completion" entry (file+headline "~/org/life.org" "Inbox")
-           "* TODO %(zp/org-protocol-get-verb \"%:link\" t) %a%? :online:\n%(zp/org-protocol-insert-selection-dwim \"%i\")" :add-created t)
+           "* TODO %(zp/org-protocol-process \"%:link\" \"%:description\" t)%? :online:\n%(zp/org-protocol-insert-selection-dwim \"%i\")" :add-created t)
 
           ;; Daily Record of Dysfunctional Thoughts
           ("D" "Record Dysfunctional Thoughts" entry (file+headline "~/org/journal.org" "Psychotherapy")
