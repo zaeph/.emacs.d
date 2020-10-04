0a09a8a8664f758fd1beea385e0148a71d6ca36e
Leo Vivier <zaeph@zaeph.net>
2020-10-04 10:10:56 +0200

    Implement compile-on-save-mode

--- init.el
+++ init.el
@@ -341,6 +341,21 @@ See `add-hooks' for details."
   (interactive "aFunction symbol: ")
   (advice-mapc (lambda (advice _props) (advice-remove sym advice)) sym))
 
+(defun compile-on-save-start ()
+  (let ((buffer (compilation-find-buffer)))
+    (unless (get-buffer-process buffer)
+      (recompile))))
+
+(define-minor-mode compile-on-save-mode
+  "Minor mode to automatically call `recompile' whenever the
+current buffer is saved. When there is ongoing compilation,
+nothing happens."
+  :lighter " CoS"
+    (if compile-on-save-mode
+    (progn  (make-local-variable 'after-save-hook)
+        (add-hook 'after-save-hook 'compile-on-save-start nil t))
+      (kill-local-variable 'after-save-hook)))
+
 ;;----------------------------------------------------------------------------
 ;; Timers
 ;;----------------------------------------------------------------------------
