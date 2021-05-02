f8e19f48763e6f0390b18213c8d4a8f331584a1a
Leo Vivier <zaeph@zaeph.net>
2021-05-02 20:17:27 +0200

    Load custom file

--- init.el
+++ init.el
@@ -52,6 +52,8 @@
 
 ;; Put customisations in a separate file
 (setq custom-file (concat user-emacs-directory "custom.el"))
+(add-to-list 'load-path custom-file)
+(load custom-file)
 
 ;; Show current filename in titlebar
 (setq frame-title-format "%b")
