5b1396b557d049fe1895d93cc6fff65f7fc858f4
Leo Vivier <zaeph@zaeph.net>
2021-05-05 13:56:03 +0200

    Move toggle-window-split to editing commands

--- init.el
+++ init.el
@@ -400,6 +400,33 @@ surrounding paragraph."
         (kill-buffer-and-window))
     (user-error "There is only one window in the frame")))
 
+;; Taken from magnars’s config
+(defun toggle-window-split ()
+  "Change the vertical split direction."
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+
 ;;----------------------------------------------------------------------------
 ;; Custom modes
 ;;----------------------------------------------------------------------------
@@ -1047,33 +1074,7 @@ With a ‘C-u’ prefix argument, amend the last commit instead."
   :bind (("s-u" . winner-undo)
          ("s-i" . winner-redo))
   :config
-  (winner-mode 1)
-
-  ;; Taken from magnars’s config
-  (defun toggle-window-split ()
-    (interactive)
-    (if (= (count-windows) 2)
-        (let* ((this-win-buffer (window-buffer))
-               (next-win-buffer (window-buffer (next-window)))
-               (this-win-edges (window-edges (selected-window)))
-               (next-win-edges (window-edges (next-window)))
-               (this-win-2nd (not (and (<= (car this-win-edges)
-                                           (car next-win-edges))
-                                       (<= (cadr this-win-edges)
-                                           (cadr next-win-edges)))))
-               (splitter
-                (if (= (car this-win-edges)
-                       (car (window-edges (next-window))))
-                    'split-window-horizontally
-                  'split-window-vertically)))
-          (delete-other-windows)
-          (let ((first-win (selected-window)))
-            (funcall splitter)
-            (if this-win-2nd (other-window 1))
-            (set-window-buffer (selected-window) this-win-buffer)
-            (set-window-buffer (next-window) next-win-buffer)
-            (select-window first-win)
-            (if this-win-2nd (other-window 1)))))))
+  (winner-mode 1))
 
 (use-package ace-link
   ;; :load-path "~/projects/ace-link/"
