d2686b4fbac14c7b08d7c641ae3a9271788aa5de
Leo Vivier <zaeph@zaeph.net>
2021-11-17 07:43:10 +0100

    theme: Tweak time slices

--- init.el
+++ init.el
@@ -4523,7 +4523,7 @@ the beginning of the line."
   (zp/set-font-variable "equity")
 
   ;; Day/night cycle
-  (setq zp/time-of-day-sections '("05:00" "08:00" "16:00" "20:00" "00:00"))
+  (setq zp/time-of-day-sections '("04:00" "05:00" "16:00" "18:00" "00:00"))
   (zp/switch-theme-auto))
 
 ;;----------------------------------------------------------------------------
