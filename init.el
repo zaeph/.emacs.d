08901fa3fb9983d611f9d3d30d7d27aedb8c1ef8
Leo Vivier <zaeph@zaeph.net>
2022-07-08 00:41:09 +0200

    Transition to vertico/embark
    Thanks to vifon for all the help!

--- init.el
+++ init.el
@@ -552,13 +552,6 @@ For details on DATA, CONTEXT, and SIGNAL, see
 ;; Disabled because of slow-downs in combination with visual-line-mode
 ;; (fcitx-aggressive-setup)
 
-(use-package consult
-  ;; :ensure t
-  :bind (([remap goto-line] . consult-goto-line)
-         ("M-g M-g" . consult-line)
-         ;; ([remap yank-pop] . consult-yank-pop)
-         ))
-
 (use-package ox-hugo)
 
 (use-package edit-indirect)
@@ -2522,6 +2515,199 @@ with effort estimates and total time."
                                                            (ditaa .t)
                                                            (awk .t))))
 
+(use-package vertico
+  :bind (("C-x M-r" . vertico-repeat)
+         :map vertico-map
+         ("C-l" . vertico-directory-delete-word)
+         ("M-g" . vertico-multiform-grid)
+         ("M-q" . vertico-multiform-flat))
+  :init (vertico-mode 1)
+  :config (progn
+            (add-hook 'minibuffer-setup-hook #'vertico-repeat-save)
+            (vertico-mouse-mode 1)
+            (vertico-multiform-mode 1)
+            (setq vertico-multiform-categories '((consult-grep buffer))
+                  vertico-multiform-commands '((tmm-menubar flat)
+                                               (tmm-shortcut flat)))))
+
+(use-package orderless
+  :after vertico
+  :config (progn
+            (setq orderless-matching-styles '(orderless-regexp
+                                              orderless-initialism
+                                              orderless-prefixes)
+                  orderless-component-separator #'orderless-escapable-split-on-space)
+
+            ;; Use the built-in "partial-completion" style to complete
+            ;; file inputs such as "/e/ni/co.nix" into
+            ;; "/etc/nixos/configuration.nix".  The "basic" style is
+            ;; needed to support the hostname completion in the TRAMP
+            ;; inputs such as "/sshx:HOSTNAME".
+            (setq completion-category-defaults nil
+                  completion-category-overrides '((file (styles basic partial-completion))))
+
+            (setq completion-styles '(orderless))
+
+            (defun vifon/orderless-without-if-bang (pattern index total)
+              (when (string-prefix-p "!" pattern)
+                `(orderless-without-literal . ,(substring pattern 1))))
+            (defun vifon/orderless-literal-if-equal (pattern index total)
+              (when (string-suffix-p "=" pattern)
+                `(orderless-literal . ,(substring pattern 0 -1))))
+            (setq orderless-style-dispatchers '(vifon/orderless-without-if-bang
+                                                vifon/orderless-literal-if-equal))))
+
+(use-package embark
+  :bind (("C-c o" . embark-act)
+         ("C-."   . embark-act)
+         :map minibuffer-local-map
+         ("M-o"   . embark-act)
+         :map embark-command-map
+         ;; Unbind the dangerous `global-set-key' and `local-set-key'
+         ;; actions.  It's far too easy to accidentally bind over some
+         ;; `self-insert-command' binding or even over
+         ;; \\[keyboard-quit].
+         ("g" . nil)
+         ("l" . nil))
+  :config (progn
+            (setq embark-mixed-indicator-delay 1)
+
+            ;; Make the eval action editable.  Evaluating code
+            ;; in-place is simple enough without Embark, if I invoke
+            ;; it with Embark, I almost definitely want to edit the
+            ;; expression beforehand.  And even if not, I can
+            ;; just confirm.
+            (cl-pushnew 'embark--allow-edit
+                        (alist-get 'pp-eval-expression embark-target-injection-hooks))
+
+            ;; Reload the project list after using
+            ;; C-u `embark-act' with `project-forget-project'.
+            ;; (cl-pushnew 'embark--restart
+            ;;             (alist-get 'project-forget-project embark-post-action-hooks))
+
+            (defun embark-act-with-eval (expression)
+              "Evaluate EXPRESSION and call `embark-act' on the result."
+              (interactive "sExpression: ")
+              (with-temp-buffer
+                (insert (eval (read expression)))
+                (embark-act)))
+
+            (dolist (keymap (list embark-variable-map embark-expression-map))
+              (define-key keymap (kbd "v") #'embark-act-with-eval))
+
+            ;; Source: https://github.com/oantolin/embark/wiki/Additional-Actions#attaching-file-to-an-email-message
+            (autoload 'gnus-dired-attach "gnus-dired" nil t)
+            (defun embark-attach-file (file)
+              "Attach FILE to an email message."
+              (interactive "fAttach: ")
+              (gnus-dired-attach (list file)))
+            (bind-key "a" #'embark-attach-file embark-file-map)))
+
+(use-package embark-consult
+  :after (embark consult))
+
+(use-package marginalia
+  :after vertico
+  :demand t                     ; :demand applies to :bind but not
+                                ; :after.  We want to eagerly load
+                                ; marginalia once vertico is loaded.
+  :bind (:map minibuffer-local-map
+         ("C-o" . marginalia-cycle))
+  :config (marginalia-mode 1))
+
+(use-package consult
+  :bind (("M-s f" . consult-line)
+         ("M-g M-g" . consult-line)
+         ("M-g o" . consult-outline)
+         ("M-g r" . consult-ripgrep)
+         ("C-x C-r" . consult-recent-file)
+         ([remap apropos-command] . consult-apropos)
+         ([remap imenu] . consult-imenu)
+         ([remap apropos] . consult-apropos)
+         ([remap switch-to-buffer] . consult-buffer)
+         ([remap yank-pop] . consult-yank-pop)
+         ([remap goto-line] . consult-goto-line)
+         :map minibuffer-local-map
+         ([remap previous-matching-history-element] . consult-history))
+  :init
+  (bind-key "TAB"
+            (lambda ()
+              (interactive)
+              (isearch-exit)
+              (let ((query (if isearch-regexp
+                               isearch-string
+                             (regexp-quote isearch-string))))
+                (consult-line query)))
+            isearch-mode-map)
+  :config
+  ;; (setq consult-project-root-function #'vc-root-dir)
+  (consult-customize
+   consult-ripgrep consult-grep
+   consult-buffer consult-recent-file
+   :preview-key (kbd "M-."))
+
+  (defun vifon/orderless-fix-consult-tofu (pattern index total)
+    "Ignore the last character which is hidden and used only internally."
+    (when (string-suffix-p "$" pattern)
+      `(orderless-regexp . ,(concat (substring pattern 0 -1)
+                                    "[\x200000-\x300000]*$"))))
+
+  (dolist (command '(consult-buffer consult-line))
+    (advice-add command :around
+                (lambda (orig &rest args)
+                  (let ((orderless-style-dispatchers (cons #'vifon/orderless-fix-consult-tofu
+                                                           orderless-style-dispatchers)))
+                    (apply orig args)))))
+
+  ;; Disable consult-buffer project-related capabilities as
+  ;; they are very slow in TRAMP.
+  (setq consult-buffer-sources
+        (delq 'consult--source-project-buffer
+              (delq 'consult--source-project-file consult-buffer-sources)))
+
+  (setq consult--source-hidden-buffer
+        (plist-put consult--source-hidden-buffer :narrow ?h)))
+
+
+;;; https://with-emacs.com/posts/tutorials/customize-completion-at-point/
+(autoload 'ffap-file-at-point "ffap")
+(add-hook 'completion-at-point-functions
+          (defun complete-path-at-point+ ()
+            (let ((fn (ffap-file-at-point))
+                  (fap (thing-at-point 'filename)))
+              (when (and (or fn (equal "/" fap))
+                         (save-excursion
+                           (search-backward fap (line-beginning-position) t)))
+                (list (match-beginning 0)
+                      (match-end 0)
+                      #'completion-file-name-table :exclusive 'no))))
+          'append)
+
+;;; Add prompt indicator to `completing-read-multiple'.
+;;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+;;;
+;;; Taken from the Vertico docs.
+(defun crm-indicator (args)
+  (cons (format "[CRM%s] %s"
+                (replace-regexp-in-string
+                 "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                 crm-separator)
+                (car args))
+        (cdr args)))
+(advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+(setq enable-recursive-minibuffers t)
+(minibuffer-depth-indicate-mode 1)
+
+;;; Use the completing-read UI for the M-tab completion unless
+;;; overridden (for example by `corfu').
+(setq-default completion-in-region-function
+              (lambda (&rest args)
+                (apply (if vertico-mode
+                           #'consult-completion-in-region
+                         #'completion--in-region)
+                       args)))
+
 ;;------------
 ;; Projectile
 ;;------------
@@ -2569,122 +2755,6 @@ with effort estimates and total time."
      ("Oa" counsel-projectile-switch-project-action-org-agenda "open project agenda")))
   :init
   (counsel-projectile-mode))
-
-;;----------------------------------------------------------------------------
-;; Helm
-;;----------------------------------------------------------------------------
-(define-prefix-command 'zp/helm-map)
-(global-set-key (kbd "C-c h") 'zp/helm-map)
-(use-package helm
-  :bind (("M-x" . helm-M-x)
-         ("<menu>" . helm-M-x)
-         ("M-y" . helm-show-kill-ring)
-         ;; ("M-y" . consult-yank-pop)
-         ;; ("s-b" . helm-mini)
-         ("C-x b" . helm-mini)
-         ("C-x C-f" . helm-find-files)
-         ("M-s M-s" . helm-occur)
-         ("C-x r b" . helm-bookmarks)
-         ("C-h C-SPC" . helm-all-mark-rings)
-         ("C-h a" . helm-apropos)
-         :map helm-map
-         ("C-S-o" . helm-previous-source)
-         :map zp/helm-map
-         (("o" . helm-occur)
-          ("f" . helm-find-files)
-          ("r" . helm-regexp)
-          ("x" . helm-register)
-          ("b" . helm-resume)
-          ("c" . helm-colors)
-          ("M-:" . helm-eval-expression-with-eldoc)
-          ("i" . helm-semantic-or-imenu)
-          ("/" . helm-find)
-          ("<tab>" . helm-lisp-completion-at-point)
-          ("p" . helm-projectile)))
-  :config
-  ;; Increase truncation of buffer names
-  (setq helm-buffer-max-length 30       ;Default: 20
-        helm-completion-style 'fuzzy)
-
-  ;; Disable helm-mode for some functions
-  ;; Used to be necessary, but now it works just fine
-  ;; (add-to-list 'helm-completing-read-handlers-alist '(org-set-property)))
-  )
-
-(use-package helm-org-rifle)
-
-;;----------------------------------------------------------------------------
-;; Selectrum
-;;----------------------------------------------------------------------------
-(use-package selectrum
-  :disabled
-  :config
-  (selectrum-mode +1))
-
-;;----------------------------------------------------------------------------
-;; Ivy
-;;----------------------------------------------------------------------------
-(use-package ivy
-  :config
-  (ivy-mode 1)
-  (setq ivy-height 10
-        ivy-use-virtual-buffers t
-        ivy-count-format "(%d/%d) ")
-
-  (global-set-key (kbd "C-c C-r") 'ivy-resume)
-  (global-set-key (kbd "<f6>") 'ivy-resume)
-
-  (setq ivy-on-del-error-function #'ignore)
-
-  (use-package hydra
-    :config
-    (setq ivy-read-action-function 'ivy-hydra-read-action))
-
-  ;; Commented because I use Helm for those commands
-  ;; (global-set-key (kbd "C-x C-b") 'ivy-switch-buffer)
-  )
-
-(use-package swiper
-  :config
-  ;; Commented because I now use counsel-grep-or-swiper
-  ;; (global-set-key "\C-s" #'swiper)
-  )
-
-(use-package counsel
-  :after swiper
-  :bind (;; ("C-s" . zp/counsel-grep-or-swiper)
-         ;; ("C-r" . counsel-grep-or-swiper-backward)
-         ;; Commented because I use the Helm equivalents
-         ;; ("M-x" . counsel-M-x)
-         ;; ("<menu>" . counsel-M-x)
-         ;; ("C-x C-f" . counsel-find-file)
-         ;; ("C-c j" . counsel-git-grep)
-         ;; Commented because unused
-         ;; ("C-c g" . counsel-git)
-         ;; ("C-c k" . counsel-ag)
-         ;; ("C-x l" . counsel-locate)
-         ;; ("C-S-o" . counsel-rhythmbox)
-         :map minibuffer-local-map
-         ("C-r" . counsel-minibuffer-history))
-  :requires swiper
-  :config
-  (setq counsel-find-file-at-point t)
-
-  ;; Use rg insted of grep
-  (setq counsel-grep-base-command "rg -i -M 120 --no-heading --line-number --color never %s %s")
-
-  (defun zp/counsel-grep-or-swiper (&optional arg)
-    "Call ‘swiper’ for small buffers and ‘counsel-grep’ for large ones.
-Wrapper to always use swiper for gpg-encrypted files and
-indirect-buffers."
-    (interactive "P")
-    (let* ((file (buffer-file-name))
-           (ext (if file (file-name-extension file))))
-      (if (or (equal arg '(4))                    ;Forcing?
-              (not file)                          ;Indirect buffer?
-              (string= ext "gpg"))                ;Encrypted buffer?
-          (swiper)
-        (counsel-grep-or-swiper)))))
 
 ;;----------------------------------------------------------------------------
 ;; Hydra
